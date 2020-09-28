612b7a48c656c234450f066cd57c67a5e0fc1314
Rohit Pandey <rohit.pandey@prolitus.com>
2020-09-28 20:16:42 +0530

    STATIC_ROOT added to settings file

--- supplier_portal/supplier_portal/settings.py
+++ supplier_portal/supplier_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
