56f1fa1ad6276bd25c5f14216292314b147756ca
Rohit Pandey <rohit.pandey@prolitus.com>
2020-09-28 20:31:25 +0530

    heroku dependencies added

--- supplier_portal/supplier_portal/settings.py
+++ supplier_portal/supplier_portal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os.path
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,3 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Heroku Settings
+django_heroku.settings(locals())
